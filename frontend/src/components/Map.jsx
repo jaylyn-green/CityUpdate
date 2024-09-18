ce20b873a2546654dc47e9dd3828aadf81c27f6e
jaylyn36 <jaylyng02@icloud.com>
2024-09-18 18:08:23 -0400

    Minor changes

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,19 +1,15 @@
 import { GoogleMap, Marker } from '@react-google-maps/api';
+import styled from "styled-components";
+
+const GoogleMap = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
 
 function MapComponent({ projects }) {
     return (
-        <GoogleMap
-            mapContainerStyle={{ height: '100vh', width: '100%' }}
-            center={{ lat: 0, lng: 0 }}  // Adjust center based on your data
-            zoom={12}
-        >
-            {projects.map((project) => (
-                <Marker
-                    key={project.id}
-                    position={{ lat: project.latitude, lng: project.longitude }}
-                    title={project.name}
-                />
-            ))}
+        <GoogleMap>
+            
         </GoogleMap>
     );
 }
